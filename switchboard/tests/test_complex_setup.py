12f565a4a45791e0515fe46b7ddd01c2c6852da8
Cyril Peponnet <cyril@peponnet.fr>
2016-01-27 08:43:19 -0800

    Fix an issue while generating haproxy configuration.

--- switchboard/tests/test_complex_setup.py
+++ switchboard/tests/test_complex_setup.py
@@ -217,3 +217,72 @@ frontend https-in-no-sni
 """
 
         self._assertEquals(name, _services, _generate_conf(_services, "tests/test.jinja2"), _expected)
+
+    def test_application_3(self):
+        name = "Application with http frontend and http frontend as well"
+        _services = [
+            {
+                "name": "myapplication-http",
+                "ip": "10.0.0.1",
+                "port": "80",
+                "id": "node1:80",
+                "tags": [
+                        "dns=domain.tld",
+                        "vhost=myapp",
+                        "check=disabled",
+                        "http=80"
+                        ]
+            },
+            {
+                "name": "myapplication-https",
+                "ip": "10.0.0.3",
+                "port": "443",
+                "id": "node3:443",
+                "tags": [
+                        "dns=domain.tld",
+                        "https=443",
+                        "vhost=myapp"
+                        ]
+            }
+        ]
+        _expected = r"""
+frontend http-in
+    bind :80 name http
+    mode http
+    reqadd X-Forwarded-Proto:\ http
+    use_backend bk_myapplication-http if { hdr(host) -i myapp.domain.tld }
+    default_backend https-redirect
+
+backend bk_myapplication-http
+    mode http
+    balance roundrobin
+    server "node1:80" 10.0.0.1:80
+
+backend bk_myapplication-https
+    mode http
+    balance roundrobin
+    option httpchk HEAD / HTTP/1.1\r\nHost:localhost
+    server "node3:443" 10.0.0.3:443 check ssl verify none
+
+backend https-redirect
+    mode http
+    redirect scheme https code 301 if !{ ssl_fc }
+
+frontend https-in
+    bind :443
+    mode tcp
+    tcp-request content accept if { req_ssl_hello_type 1 }
+    default_backend https-no-sni
+
+backend https-no-sni
+    mode tcp
+    server "https-no-sni-loop" localhost:10443
+
+frontend https-in-no-sni
+    mode http
+    bind localhost:10443 ssl crt /etc/haproxy/certs/
+    reqadd X-Forwarded-Proto:\ https
+    use_backend bk_myapplication-https if { hdr(host) -i myapp.domain.tld }
+"""
+
+        self._assertEquals(name, _services, _generate_conf(_services, "tests/test.jinja2"), _expected)
